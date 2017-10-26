6f4f6e70aff90b86d2c732b67184045c3f59dbcb
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-10-26 19:02:53 -0400

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-
+  validates :username, uniqueness: :true
   has_many :packs
   has_many :pack_items, through: :packs
   has_many :items, through: :pack_items
