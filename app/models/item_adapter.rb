212e8214813eddf63611e040d466baba903dbb03
Ariel Herman <aeh.herman@gmail.com>
2017-10-26 01:50:10 -0400

    Hitting API instead of DB for new item instances

--- app/models/item_adapter.rb
+++ app/models/item_adapter.rb
@@ -2,7 +2,8 @@ class ItemAdapter < ApplicationRecord
 
   KEY = "mxo98hn36jc699lhyusekstd"
   # ROUTE = "https://openapi.etsy.com/v2/listings/active?api_key=#{KEY}&keywords="
-  ROUTE="https://openapi.etsy.com/v2/listings/active?includes=Images(url_170x135)&fields=title,price,description,url&limit=20&api_key=#{KEY}&keywords="
+  ROUTE="https://openapi.etsy.com/v2/listings/active?includes=Images(url_170x135)&fields=title,price,description,url&limit=1&api_key=#{KEY}&keywords="
+
   SECRET = "0jfed3h7m4"
 
   def self.get_route
@@ -11,26 +12,21 @@ class ItemAdapter < ApplicationRecord
 
   def self.get_results(keyword)
     response = RestClient.get("#{ROUTE}#{keyword}")
-    JSON.parse(response)["results"]
+    JSON.parse(response)["results"][0]
     # returns an array of hashes
   end
 
   # call this method in the seeds file
   # we can use color in here too if we want!
   def self.import(keywords_ary)
-    keywords_ary.each do |keyword|
-      item_ary = ItemAdapter.get_results(keyword)
-      item_ary.each do |item_hash|
-        item = Item.new
-        item.title = item_hash["title"]
-        item.price = item_hash["price"]
-        item.description = item_hash["description"]
-        item.url = item_hash["url"]
-        item.image = item_hash["Images"][0]["url_170x135"]
-        item.keyword = keyword
-        item.save
-      end
+    #  byebug
+    keywords_ary.map do |keyword|
+      result = get_results(keyword)
+      Item.new_from_api(result)
     end
+     # this line is an array of hashes that can be used to make new items
+    # results_ary.map {|item_hash| Item.new_from_api(item_hash) }
+    # this returns an array of hashes that can instantiate new item objects
   end
 
 
