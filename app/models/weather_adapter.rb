7066275817867200657c415519e3f8a99e8b466b
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-01-11 14:49:17 -0500

    fixed weather api

--- app/models/weather_adapter.rb
+++ app/models/weather_adapter.rb
@@ -1,8 +1,7 @@
 class WeatherAdapter < ApplicationRecord
   require 'open-uri'
   def self.local_xml(location)
-
-      url1 = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=#{location}&format=xml&num_of_days=5&key=acdcc7e8951d4de190d165134172310"
+      url1  = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=#{location}&format=xml&num_of_days=5&key=#{ENV["weather_key"]}"
       Nokogiri::XML(open(url1))
   end
 
